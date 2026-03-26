0c00981b9aca8a0eb4327140e6741c7b4ce10105
Abril Herrera <AbrilHerrera00@outlook.com>
2026-03-25 20:03:46 -0700

    ADD: Appointment DTO

---
+++ U2P3-PetsAPI/Models/DTO/AppointmentDTO.cs
@@ -0,0 +1,24 @@
+﻿namespace U2P3_PetsAPI.Models.DTO
+{
+    public class AppointmentDTO
+    {
+        public int AppointmentId { get; set; }
+        public DateTime AppointmentDate { get; set; }
+        public string Reason { get; set; }
+
+        // Pet
+        public int PetId { get; set; }
+        public string PetName { get; set; }
+        public string Species { get; set; }
+
+        // Owner
+        public int OwnerId { get; set; }
+        public string OwnerName { get; set; }
+
+        // Veterinarian
+        public int VetId { get; set; }
+        public string VetName { get; set; }
+        public string Specialty { get; set; }
+
+    }
+}
