f7d95b15f5b5efd6b189ef981222db7fd27bdcb0
Heidy <penuelasheidysec@gmail.com>
2026-04-07 21:17:15 -0700

    ADD: VeterinariansDTO

---
+++ U2P3-PetsAPI/Models/DTO/VeterinariansDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace U2P3_PetsAPI.Models.DTO
+{
+    public class VeterinariansDTO
+    {
+        // Veterinarian
+        public int VetId { get; set; }
+        public string VetName { get; set; }
+        public string Specialty { get; set; }
+    }
+}
